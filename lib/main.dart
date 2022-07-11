0697aeecb56f04f6dbc46e3e14b25694d3904938
Gabriel Rodrigues <gabriel.rodrigues.mota@outlook.com>
2022-07-10 21:33:56 -0300

    Exemplo simples de como usar o mobx para gerenciar o estado sobre o projeto inicial gerado pelo flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:pomodoro/store/contador.store.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,34 +17,20 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(title: 'Mobx'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
+  HomePage({Key? key, required this.title}) : super(key: key);
+  ContadorStore store = ContadorStore();
+  String title;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -51,15 +39,19 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Observer(
+              builder: (_) {
+                return Text(
+                  '${store.contador}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: store.increment,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
---
+++ lib/store/contador.store.g.dart
@@ -0,0 +1,34 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'contador.store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
+
+mixin _$ContadorStore on _ContadorStore, Store {
+  late final _$contadorAtom =
+      Atom(name: '_ContadorStore.contador', context: context);
+
+  @override
+  int get contador {
+    _$contadorAtom.reportRead();
+    return super.contador;
+  }
+
+  @override
+  set contador(int value) {
+    _$contadorAtom.reportWrite(value, super.contador, () {
+      super.contador = value;
+    });
+  }
+
+  @override
+  String toString() {
+    return '''
+contador: ${contador}
+    ''';
+  }
+}
